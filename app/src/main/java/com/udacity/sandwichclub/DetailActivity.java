072e1aec8cb05024c8b8abf2e346e75b3331279e
Torben Möller <torben.moeller@gmx.net>
2018-02-18 15:59:44 +0100

    UI polishing

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Layout;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,6 +13,10 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -59,19 +65,29 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI(Sandwich sandwich) {
-        TextView origin = findViewById(R.id.origin_tv);
-        origin.setText(sandwich.getPlaceOfOrigin());
+        populateTextView(R.id.header_origin_tv, R.id.origin_tv, sandwich.getPlaceOfOrigin());
 
-        TextView alsoknown = findViewById(R.id.also_known_tv);
-        alsoknown.setText(sandwich.getAlsoKnownAs().toString());
+        populateTextView(R.id.header_description_tv, R.id.description_tv, sandwich.getDescription());
 
-        TextView ingredients = findViewById(R.id.ingredients_tv);
-        ingredients.setText(sandwich.getIngredients().toString());
+        String alsoknownText = concatList(sandwich.getAlsoKnownAs());
+        populateTextView(R.id.header_also_known_tv, R.id.also_known_tv, alsoknownText);
 
-        TextView description = findViewById(R.id.description_tv);
-        description.setText(sandwich.getDescription());
+        String ingredientsText = concatList(sandwich.getIngredients());
+        populateTextView(R.id.header_ingredients_tv, R.id.ingredients_tv, ingredientsText);
+    }
 
+    private void populateTextView(int headerTextViewID, int contentTextViewID, String content){
+        TextView headertTextView = findViewById(headerTextViewID);
+        TextView contentTextView = findViewById(contentTextViewID);
+        contentTextView.setText(content);
+        //Thanks to forum: https://discussions.udacity.com/t/hide-views-sandwich-club/599464
+        if(TextUtils.isEmpty(content)){
+            headertTextView.setVisibility(View.GONE);
+            contentTextView.setVisibility(View.GONE);
+        }
+    }
 
-
+    private static String concatList(List<String> list) {
+        return TextUtils.join(", ", list);
     }
 }
